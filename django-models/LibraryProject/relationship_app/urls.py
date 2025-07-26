7e800207e337e9cde3fce5208202a585854c09ce
KELVIN <kelvinkipyegon568@gmail.com>
2025-07-26 23:49:41 +0300

    refactor: update auth URL patterns for register, login, and logout views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,26 +1,26 @@
 from django.urls import path
-from .views import home_view, list_books, register_view, login_view, logout_view, LibraryDetailView
+from . import views
 from .views_ext.admin_view import admin_view
 from .views_ext.librarian_view import librarian_view
 from .views_ext.member_view import member_view
 from django.contrib.auth import views as auth_views
-from .views import add_book, edit_book, delete_book
-
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 
 
 urlpatterns = [
-    path('', home_view, name= 'home' ),
-    path('books/', list_books, name= 'list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('', views.home_view, name= 'home' ),
+    path('books/', views.list_books, name= 'list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
+    path('register/', views.register, name='register'),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
     path('accounts/login/', auth_views.LoginView.as_view(), name= 'login'),
-    path('books/add/', add_book, name='add_book'),
-    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
 ]
