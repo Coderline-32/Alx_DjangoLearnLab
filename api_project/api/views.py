bea65a7061f512a281c1d1c19223aee0e4d4ee9d
KELVIN <kelvinkipyegon568@gmail.com>
2025-08-05 09:42:17 +0300

    [feature/auth-permissions] Add token authentication and permission controls to secure API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
@@ -11,3 +12,4 @@ class BookList(ListAPIView):
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
