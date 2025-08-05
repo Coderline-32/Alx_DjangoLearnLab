d518d07161d6c853e6b53d084d2fed20bbc58b71
KELVIN <kelvinkipyegon568@gmail.com>
2025-08-05 09:20:54 +0300

    feat: - Removed confusing directory api_project completely so as to start off - Setup api-project (django-admin startproject) - Installed virtual environment(in main directory- ALX-Django LearnLab) - Installed django rest framework - Started app 'api' in api_project

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fdhztpqhvpn@)+^1tj$-$iq^m7+(jv&1^%f1v4v-4o!$b(m&@b'
+SECRET_KEY = 'django-insecure-ehgjz0!n91x0-bxg9!-u(4m@nq)^zkvd52vu&06gfplp0n22=-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'api',
 ]
 
 MIDDLEWARE = [
