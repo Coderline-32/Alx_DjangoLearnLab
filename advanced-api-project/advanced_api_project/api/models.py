a0281a87a7325d1b34e6e129e7272af0f2c1bdab
KELVIN <kelvinkipyegon568@gmail.com>
2025-08-09 12:45:30 +0300

    Added validation checker for date to ensure publication date cannott be today

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Author(models.Model):
+    """
+    Represents an author of books.
+    """
     name = models.CharField(max_length=100)
 
 class Book(models.Model):
+    """
+    Represents a book written by an author.
+    """
+     
     title = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    publication_year = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author, related_name='book', on_delete=models.CASCADE)
 
--- advanced-api-project/advanced_api_project/api/serializers.py
+++ advanced-api-project/advanced_api_project/api/serializers.py
@@ -1,15 +1,32 @@
 from rest_framework import serializers
 from .models import Author, Book
+from datetime import date
 
 class BookSerializer(serializers.ModelSerializer):
     
+    """
+    Serializes all fields of the Book model.
+    Adds custom validation to ensure the publication_date is not in the future.
+    """
     class Meta:
         model = Book
-        fields = ['title', 'publication_year', 'author']
-
+        fields = '__all__'
+    def validate_publication_date(self, value):
+        """
+        Custom validation for the publication_date field.
+        Ensures the publication date is not in the future.
+        """
+        if value > date.today():
+            raise serializers.ValidationError('Publication date cannot be current date')
+        return value
+    
 class AuthorSerializer(serializers.ModelSerializer):
+    """
+    Serializes the Author model, including the 'name' field
+    and a nested serialization of related books using BookSerializer.
+    """
 
-    books = BookSerializer(many=True, read_noly=True, source='books')
+    books = BookSerializer(many=True, read_only=True, source='books')
     class Meta:
         model = Author
-        fields = ['name']
+        fields = ['name', 'books']
