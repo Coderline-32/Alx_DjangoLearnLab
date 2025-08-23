526ad63cb36c4ea5a92cab05a0f74a5380ef36bf
KELVIN <kelvinkipyegon568@gmail.com>
2025-08-24 00:54:01 +0300

    feat: initial Django blog project setup
    - Installed Django and created project `django_blog`
    - Added new app `blog` and registered it in INSTALLED_APPS
    - Configured database (default SQLite)
    - Created `Post` model with fields: title, content, published_date, author
    - Ran initial migrations for `blog` app
    - Set up static and templates directories in `blog`
    - Added base HTML/CSS/JS files for initial structure
    - Verified setup by running development server

---
+++ django_blog/blog/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from taggit.managers import TaggableManager
+
+class Post(models.Model):
+    """
+    Represents a blog post with a title, content, author, publication date,
+    and tags.
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        """
+        Returns the URL to access a particular instance of the post.
+        """
+        return reverse('blog:post_detail', kwargs={'pk': self.pk})
+
+class Comment(models.Model):
+    """
+    Represents a comment made on a blog post.
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+
+    class Meta:
+        ordering = ['created_at']
